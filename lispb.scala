f2f874143d932ff8b04cec8b1baf3f1d00ed4782
Nada Amin <na482@cl.cam.ac.uk>
2018-08-23 15:35:07 +0100

    simplifying by having primitive values use the same format as lambda

--- lispb.scala
+++ lispb.scala
@@ -2,99 +2,25 @@ package lispb
 
 object ast {
   trait Value
-  case class I(n: Int) extends Value
-  case class B(b: Boolean) extends Value
-  case class S(sym: String) extends Value
-  case object N extends Value
-  case class P(car: Value, cdr: Value) extends Value
-  case class Clo(params: Value, body: Value, env: Env) extends Value
-  type Env = List[scala.collection.mutable.Map[String,Value]]
-
-  def apply_primitive_on_values(p: String, args: List[Value]): Value = (p, args) match {
-    case ("null?", List(a)) => B(a==N)
-    case ("number?", List(a)) => B(a match {
-      case I(_) => true
-      case _ => false
-    })
-    case ("pair?", List(a)) => B(a match {
-      case P(_, _) => true
-      case _ => false
-    })
-    case ("symbol?", List(a)) => B(a match {
-      case P(_, _) => true
-      case _ => false
-    })
-    case ("<", List(I(a), I(b))) => B(a < b)
-    case ("+", List(I(a), I(b))) => I(a+b)
-    case ("-", List(I(a), I(b))) => I(a-b)
-    case ("*", List(I(a), I(b))) => I(a*b)
-    case ("car", List(v)) => car(v)
-    case ("cdr", List(v)) => cdr(v)
-    case ("cons", List(a, d)) => cons(a, d)
-    case ("eq?", List(a, b)) => B(a==b)
-    case ("display", List(a)) => display(a); I(0)
-    case ("newline", Nil) => newline(); I(0)
-  }
-
-  def cons(car: Value, cdr: Value) = P(car, cdr)
-  def car(v: Value) = v match {
-    case P(a, d) => a
-  }
-  def cdr(v: Value) = v match {
-    case P(a, d) => d
-  }
-
-  def addParen(p: (Boolean, String)) = {
-    val (need_paren, s) = p
-    if (need_paren) "("+s+")" else s
-  }
-  def pp(v: Value): (Boolean, String) = v match {
-    case B(b) => (false, if (b) "#t" else "#f")
-    case I(n) => (false, n.toString)
-    case S(s) => (false, s)
-    case N => (true, "")
-    case P(a, N) => (true, addParen(pp(a)))
-    case P(a, d) =>
-      val s1 = addParen(pp(a))
-      val (need_paren2, s2) = pp(d)
-      if (need_paren2) (true, s1+" "+s2)
-      else (true, s1+" . "+s2)
-    case _ => (false, v.toString)
-  }
-  def show(v: Value) = addParen(pp(v))
-  def display(v: Value) = print(show(v))
-  def newline() = println("")
-
-  def prims = Set(
-    "null?",
-    "number?",
-    "pair?",
-    "symbol?",
-    "<",
-    "+",
-    "-",
-    "*",
-    "car",
-    "cdr",
-    "cons",
-    "eq?",
-    "display",
-    "newline")
+  case object Undefined extends Value
+  case class B(b: Boolean) extends Value                      // Boolean
+  case class I(n: Int) extends Value                          // Int
+  case class S(sym: String) extends Value                     // Symbol
+  case object N extends Value                                 // Nil
+  case class P(car: Value, cdr: Value) extends Value          // Pair
+  case class F(f: Value => Value) extends Value               // Procedures
+  type Frame = scala.collection.mutable.Map[String,Value]
+  type Env = List[Frame]
+  type Cont = Value => Value
 
   def list(e: Value): List[Value] = e match {
     case N => Nil
     case P(first, rest) => first :: list(rest)
   }
-  def valueOf(es: List[Value]): Value = es match {
-    case Nil => N
-    case first::rest => P(first, valueOf(rest))
-  }
 }
 
 import ast._
 object eval {
-  type Cont = Value => Value
-
   def base_eval(exp: Value, env: Env, cont: Cont): Value = {
     exp match {
       case I(_) | B(_) => cont(exp)
@@ -112,15 +38,18 @@ object eval {
   def eval_var(exp: Value, env: Env, cont: Cont): Value = exp match {
     case S(x) => get(env, x) match { case Some(v) => cont(v) }
   }
+
   def eval_quote(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(_, P(x, N)) => cont(x)
   }
+
   def eval_if(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(_, P(c, P(a, P(b, N)))) => base_eval(c, env, { cv => cv match {
       case B(false) => base_eval(b, env, cont)
       case B(true) => base_eval(a, env, cont)
     }})
   }
+
   def eval_set_bang(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(_, P(S(x), P(rhs, N))) => base_eval(rhs, env, { v =>
       set(env, x, v) match { case Some(v) => cont(v) }
@@ -128,38 +57,40 @@ object eval {
   }
 
   def eval_lambda(exp: Value, env: Env, cont: Cont): Value = exp match {
-    case P(_, P(params, body)) => cont(Clo(params, body, env))
+    case P(_, P(params, body)) => cont(F({args =>
+      eval_begin(body, extend(env, params, args), {v => v})
+    }))
   }
+
   def eval_begin(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(e, N) => base_eval(e, env, cont)
     case P(e, es) => base_eval(e, env, { _ => eval_begin(es, env, cont) })
   }
+
   def eval_define(exp: Value, env: Env, cont: Cont): Value = exp match {
     case P(_, P(r@S(name), body)) => {
-      env.head(name) = I(0)
+      env.head(name) = Undefined
       eval_begin(body, env, {v =>
         env.head(name) = v
         cont(r)})
     }
   }
+
   def eval_application(exp: Value, env: Env, cont: Cont): Value = exp match {
-    case P(fun, args) => evlist(args, env, { vas => fun match {
-      case S(p) if prims.contains(p) => cont(apply_primitive_on_values(p, vas))
-      case _ => base_eval(fun, env, { vf =>
-        val Clo(params, body, clo_env) = vf
-        val ps = list(params).map{x => x.asInstanceOf[S].sym}
-        eval_begin(body, extend(clo_env, ps, vas), cont)
-      })
+    case P(fun, args) => base_eval(fun, env, { vf => vf match {
+      case F(f) => evlist(args, env, { vas => cont(f(vas)) })
     }})
   }
 
-  def evlist(exp: Value, env: Env, k: List[Value] => Value): Value = exp match {
-    case N => k(Nil)
-    case P(first, rest) => base_eval(first, env, {v => evlist(rest, env, {vs => k(v::vs)})})
+  def evlist(exp: Value, env: Env, cont: Value => Value): Value = exp match {
+    case N => cont(N)
+    case P(first, rest) => base_eval(first, env, {v => evlist(rest, env, {vs => cont(P(v,vs))})})
   }
 
-  def extend(env: Env, params: List[String], args: List[Value]): Env =
-    collection.mutable.Map(((params zip args) : _*))::env
+  def extend(env: Env, params: Value, args: Value): Env = {
+    val frame: Frame = collection.mutable.Map((list(params).map(_.asInstanceOf[S].sym) zip list(args)) : _*)
+    frame::env
+  }
 
   def get(env: Env, x: String): Option[Value] = env match {
     case Nil => None
@@ -180,7 +111,11 @@ object eval {
     }
   }
 
-  def init_env: Env = (scala.collection.mutable.Map[String,Value]()) :: (Nil: Env)
+  def init_env: Env = (scala.collection.mutable.Map[String,Value](
+    ("<" ->   F({args => args match { case P(I(a), P(I(b), N)) => B(a<b) }})),
+    ("*" ->   F({args => args match { case P(I(a), P(I(b), N)) => I(a*b) }})),
+    ("-" ->   F({args => args match { case P(I(a), P(I(b), N)) => I(a-b) }})),
+    ("eq?" -> F({args => args match { case P(a, P(b, N)) => B(a==b) }})))) :: (Nil: Env)
 }
 
 import scala.util.parsing.combinator._
@@ -212,6 +147,29 @@ object repl {
   def clean() = {
     global_env = init_env
   }
+}
+
+object pp {
+  def addParen(p: (Boolean, String)) = {
+    val (need_paren, s) = p
+    if (need_paren) "("+s+")" else s
+  }
+  def pp(v: Value): (Boolean, String) = v match {
+    case B(b) => (false, if (b) "#t" else "#f")
+    case I(n) => (false, n.toString)
+    case S(s) => (false, s)
+    case N => (true, "")
+    case P(a, N) => (true, addParen(pp(a)))
+    case P(a, d) =>
+      val s1 = addParen(pp(a))
+      val (need_paren2, s2) = pp(d)
+      if (need_paren2) (true, s1+" "+s2)
+      else (true, s1+" . "+s2)
+    case _ => (false, v.toString)
+  }
+  def show(v: Value) = addParen(pp(v))
+  def display(v: Value) = print(show(v))
+  def newline() = println("")
 }
 
 import repl._
