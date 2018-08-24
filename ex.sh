9f0cf3e785180421184034b690795fcbaf71c8c7
Nada Amin <na482@cl.cam.ac.uk>
2018-08-24 23:25:57 +0100

    out files

--- ex.sh
+++ ex.sh
@@ -8,10 +8,15 @@ cp DO.md ex1/README.md
 zip -r ex1.zip ex1
 
 mkdir -p ex2
+mkdir -p ex2/out
 cp ex2_lisp.scala ex2/lisp0.scala
 cd ex2
 perl -pi -e 's/ex2_lisp/lisp0/g' lisp0.scala
 cd ..
+cp out/factorial.check.c ex2/out/
+cp out/factorial.check.scala ex2/out/
+cp out/odd.check.c ex2/out/
+cp out/odd.check.scala ex2/out/
 cp build.sbt ex2/build.sbt
 cp lms.scala ex2/lms.scala
 cp utils.scala ex2/utils.scala
